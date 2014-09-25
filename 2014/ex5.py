d998c2c2f45bae1a87d39f98c382c35dbda2057d
Brandon Freeman <brandon.freeman@explorys.com>
2014-09-25 16:42:04 -0400

    bfreeman update ex5.py extra

--- 2014/ex5.py
+++ 2014/ex5.py
@@ -1,20 +1,20 @@
 __author__ = 'brandon'
 
-my_name = 'Brandon Freeman'
-my_age = 100
-my_height = 80
-my_weight = 205
-my_eyes = 'Brown'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Brandon Freeman'
+age = 100
+height = 80
+weight = 205
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Brown'
 
-print "Let's talk about %s" % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "Let's talk about %s" % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair" % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %s hair" % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d." % (
-    my_age, my_height, my_weight, my_age + my_height + my_weight)
+    age, height, weight, age + height + weight)
