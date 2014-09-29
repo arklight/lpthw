815dc806680917040aa32e7b4b57990575650ca4
arklight <prohobbs@gmail.com>
2014-09-29 10:03:17 -0400

    bfreeman ex19 and ex20

--- 2014/ex19.py
+++ 2014/ex19.py
@@ -1,6 +1,6 @@
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
-    print "You have %d cheeses!" % cheese_count
-    print "You have %d boxes of crackers!" % boxes_of_crackers
+    print "You have %r cheeses!" % cheese_count
+    print "You have %r boxes of crackers!" % boxes_of_crackers
     print "Man that's enough for a party!"
     print "Get a blanket.\n"
 
@@ -19,3 +19,8 @@ cheese_and_crackers(10 + 20, 5 + 6)
 
 print "And we can combine the two, variables and math:"
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
+cheese_input = raw_input("How much cheese you have?: ")
+crackers_input = raw_input("How many crackers you have?: ")
+
+cheese_and_crackers(cheese_input, crackers_input)
